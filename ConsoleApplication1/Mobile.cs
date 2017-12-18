8f8acda9f4d1e696fee0cba3361ddbadaa809ac2
OLKL <OLKL@DK01WV2167>
2017-12-18 13:36:08 +0100

    Add project files.

---
+++ ConsoleApplication1/Mobile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhone
+{
+    public abstract class Mobile
+    {
+        public abstract ScreenBase Screen { get; }
+        public abstract PhoneDisplay Display { get; }
+        public abstract Keyboard Keyboard { get; }
+        public abstract Battery Battery { get; }
+        public abstract SimCard SimCard { get; }
+        public abstract OperatingSystem OperatingSystem { get; }
+        public abstract Camera Camera { get; }
+
+        private void Show(IScreenImage screenImage)
+        {
+            Screen.Show(screenImage);
+        }
+
+        public string Description()
+        {
+            var descriptionBuilder = new StringBuilder();
+            descriptionBuilder.AppendLine($"Screen Type:\n{Screen.ToString()}");
+            descriptionBuilder.AppendLine();
+            descriptionBuilder.AppendLine($"Display Parameters:\n{Display.ToString()}");
+            descriptionBuilder.AppendLine();
+            descriptionBuilder.AppendLine($"Keyboard parmeters:\n{Keyboard.ToString()}");
+            descriptionBuilder.AppendLine();
+            descriptionBuilder.AppendLine($"Battery parameters:\n{Battery.ToString()}");
+            descriptionBuilder.AppendLine();
+            descriptionBuilder.AppendLine($"SimCard parameters:\n{SimCard.ToString()}");
+            descriptionBuilder.AppendLine();
+            descriptionBuilder.AppendLine($"Operating system parameters:\n{OperatingSystem.ToString()}");
+            descriptionBuilder.AppendLine();
+            descriptionBuilder.AppendLine($"Camera parameters:\n{Camera.ToString()}");
+            return descriptionBuilder.ToString();
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimCorpMobile MyMobile = new MobilePhone.SimCorpMobile();
+            Console.WriteLine(MyMobile.Description());
+            Console.ReadKey();
+        }
+    }
+}
